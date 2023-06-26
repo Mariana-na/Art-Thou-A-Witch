593b5f7e6712d4b31c37986169631362305879e5
Mariana Ribeiro de Carvalho <mariana.rcarvalho.992gmail.com>
2023-06-26 12:19:28 +0100

    lunch break

--- JS/game.js
+++ JS/game.js
@@ -1,1 +1,39 @@
-f
+class Game {
+    constructor() {
+      this.startScreen = document.getElementById('startScreen')
+      this.gameScreen = document.getElementById('gameScreen')
+      this.gameEndScreen = document.getElementById('gameOver')
+      this.height = 600
+      this.width = 500
+      this.player = null
+      this.obstacles = []
+      this.isGameOver = false
+      this.animateId
+    }
+    start (){
+        this.gameScreen.style.width = `${this.width}px`;
+        this.gameScreen.style.height = `${this.height}px`;
+
+        this.startScreen.style.display = "none";
+        this.gameScreen.style.display = "block";
+
+        this.gameloop();
+    }
+
+    gameLoop() {
+        console.log("in the game loop");
+    
+        // Interrupt the function to stop the loop if "gameIsOver" is set to "true"
+        if (this.gameIsOver) {
+          return;
+        }
+    
+        this.update();
+    
+        window.requestAnimationFrame(() => this.gameLoop());
+      }
+    
+      update() {
+        console.log("in the update");
+      }
+}
--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,15 @@
-j
+window.addEventListener("load", ()=> {
+    const startBtn = document.getElementById("startBtn");
+    const restartBtn = document.getElementById("reStartBtn");
+    let game;
+
+    function startGame (){
+        console.log("start game");
+        game = new Game();
+        game.start();
+    }
+
+    startBtn.addEventListener("click", function (){
+        startGame();
+    });
+});
